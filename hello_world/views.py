85358669cd62fd1f1a430d17bffbc36bd23bf31f
Christopher Meddings <c.m.meddings@gmail.com>
2024-03-10 13:06:12 +0000

    Added code to display request method

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world")
+
+    if request.method == 'POST':
+        return HttpResponse("This was a POSTed something")
+
+    else:
+        return HttpResponse(request.method)
