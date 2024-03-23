ea5ccf60715179b399688340f6dca425331f1188
Christopher Meddings <c.m.meddings@gmail.com>
2024-03-23 18:27:07 +0000

    Add JSON file with post data

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,17 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status','author',)
+    # This display the fields on the table
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # This allows the user to search for content in the desired fields - i.e title
+    # This speeds up the searching
+    search_fields = ['title','content']
+    # This provides the filters to be applied. I.e. by author or status (publihsed or draft)
+    list_filter = ('status','created_on',)
+    # This function now automatically creates the slug
+    # slug then the field to be created into a slug
     prepopulated_fields = {'slug': ('title',)}
+    # this add the rish text editor to the field that is passed.
     summernote_fields = ('content',)
 
 # Register your models here.
